dd094a8b6725bc03b6f34da55845e97af19c1fef
Prateek Gupta <prateekashish30@gmail.com>
2025-10-03 10:31:26 +0530

    Create babysoc_top.v

---
+++ Week2 (understanding SoC concepts)/VSDBabySoC_project/verilog_files/babysoc_top.v
@@ -0,0 +1,39 @@
+module babysoc_top (
+   output wire OUT,
+   //
+   input  wire reset,
+   //
+   input  wire VCO_IN,
+   input  wire ENb_CP,
+   input  wire ENb_VCO,
+   input  wire REF,
+   //
+   // input  wire VREFL,
+   input  wire VREFH
+);
+
+   wire CLK;
+   wire [9:0] RV_TO_DAC;
+
+   rvmyth core (
+      .OUT(RV_TO_DAC),
+      .CLK(CLK),
+      .reset(reset)
+   );
+
+   pll_module pll (
+      .CLK(CLK),
+      .VCO_IN(VCO_IN),
+      .ENb_CP(ENb_CP),
+      .ENb_VCO(ENb_VCO),
+      .REF(REF)
+   );
+
+   dac_module dac (
+      .OUT(OUT),
+      .D(RV_TO_DAC),
+      // .VREFL(VREFL),
+      .VREFH(VREFH)
+   );
+   
+endmodule
